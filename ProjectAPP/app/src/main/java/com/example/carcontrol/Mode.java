d3fd80cd8c2a6a34fabf196144792e9174005a7b
JaneDamsgaardNielsen <74055422+JaneDamsgaardNielsen@users.noreply.github.com>
2021-01-13 16:37:20 +0100

    layout of buttons
    now the layouts of all the buttons is woeking

--- ProjectAPP/app/src/main/java/com/example/carcontrol/Mode.java
+++ ProjectAPP/app/src/main/java/com/example/carcontrol/Mode.java
@@ -8,9 +8,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class Mode extends AppCompatActivity {
-
+    Button auto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
+
+        auto = findViewById(R.id.auto);
+
+        auto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Mode.this, auto.class);
+                startActivity(intent);
+            }
+        });
 }}
