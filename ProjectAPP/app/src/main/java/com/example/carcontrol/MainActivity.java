196a13ba145059589b5663b97f7038775e6be946
JaneDamsgaardNielsen <74055422+JaneDamsgaardNielsen@users.noreply.github.com>
2021-01-13 16:10:55 +0100

    most buttons is working
    Exit, Mode, back is working.
    Auto and manuel not setup

--- ProjectAPP/app/src/main/java/com/example/carcontrol/MainActivity.java
+++ ProjectAPP/app/src/main/java/com/example/carcontrol/MainActivity.java
@@ -1,19 +1,45 @@
 package com.example.carcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    Button exit, mode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mode = findViewById(R.id.modeButton);
+
+        mode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Mode.class);
+                startActivity(intent);
+            }
+        });
+        exit = findViewById(R.id.ExitButton);
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               finish();
+            }
+        });
+
     }
     public void ShowGif(View view) {
         ImageView imageView = findViewById(R.id.imageView);
         Glide.with(this).load(R.drawable.giphy).into(imageView);
     }
+
+
+
 }
--- ProjectAPP/app/src/main/java/com/example/carcontrol/Mode.java
+++ ProjectAPP/app/src/main/java/com/example/carcontrol/Mode.java
@@ -2,7 +2,10 @@ package com.example.carcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Mode extends AppCompatActivity {
 
@@ -10,5 +13,4 @@ public class Mode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
-    }
-}
+}}
