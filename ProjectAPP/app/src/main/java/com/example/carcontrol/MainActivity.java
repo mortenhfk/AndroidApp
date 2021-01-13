3b44baf77dedf4c25d28ecc586bc2bfeb848de77
Morten <morten.h.f.k@gmail.com>
2021-01-13 09:38:24 +0100

    Frontpage

--- ProjectAPP/app/src/main/java/com/example/carcontrol/MainActivity.java
+++ ProjectAPP/app/src/main/java/com/example/carcontrol/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.carcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void ShowGif(View view) {
+        ImageView imageView = findViewById(R.id.imageView);
+        Glide.with(this).load(R.drawable.giphy).into(imageView);
     }
 }
---
+++ ProjectAPP/app/src/main/java/com/example/carcontrol/Mode.java
@@ -0,0 +1,14 @@
+package com.example.carcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode);
+    }
+}
